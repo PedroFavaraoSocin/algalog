ce6b6002dd486925644b6644fb535a590f595741
pedro.favarao <pedro.favarao@socin.com.br>
2023-08-03 18:50:20 -0300

    Primeiro commit

---
+++ src/main/java/com/algaworks/algalog/AlgalogApi1Application.java
@@ -0,0 +1,13 @@
+package com.algaworks.algalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgalogApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgalogApi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/algalog/AlgalogApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgalogApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
