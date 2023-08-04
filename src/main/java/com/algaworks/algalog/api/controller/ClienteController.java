bd9a0ddd0291f19c6a1f3ceb10cae3504d821a54
pedro.favarao <pedro.favarao@socin.com.br>
2023-08-04 01:26:03 -0300

    Inclusão dos metodos listar, buscar por ID e adicionar cliente

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,20 +1,54 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 	
-	@GetMapping("/clientes")
-	public List<Cliente> teste(){
-		Cliente cliente1 = new Cliente(1L, "Pedro", "pedrofavarao@gmail.com", "(16)99322-3662");
-		Cliente cliente2 = new Cliente(2L, "João", "joao.favarao@gmail.com", "(16)99322-3662");
-		return Arrays.asList(cliente1, cliente2);
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@GetMapping
+	public List<Cliente> listar(){
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId){
+		return clienteRepository.findById(clienteId)
+//				.map(cliente -> ResponseEntity.ok(cliente))
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+		
+//		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+//		if (cliente.isPresent()) {
+//			return ResponseEntity.ok(cliente.get());
+//		}
+//		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
 	}
 }
--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,25 +1,33 @@
 package com.algaworks.algalog.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
+@Table(name = "cliente")
 public class Cliente {
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "nome")
 	private String nome;
+	
+	@Column(name = "email")
 	private String email;
+	
+	@Column(name = "telefone")
 	private String telefone;
-	
-	public Cliente() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public Cliente(Long id, String nome, String email, String telefone) {
-		// TODO Auto-generated constructor stub
-		this.id = id;
-		this.nome = nome;
-		this.email = email;
-		this.telefone = telefone;
-	}
+
 }
