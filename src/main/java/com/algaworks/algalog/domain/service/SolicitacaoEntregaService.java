d06a7487c304064ea6abb33ce2c63339cac6ff0e
pedro.favarao <pedro.favarao@socin.com.br>
2023-08-15 12:20:35 -0300

    Inclusão da entrega de pedidos

---
+++ src/main/java/com/algaworks/algalog/domain/service/SolicitacaoEntregaService.java
@@ -0,0 +1,28 @@
+package com.algaworks.algalog.domain.service;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.algaworks.algalog.domain.model.Entrega;
+import com.algaworks.algalog.domain.model.StatusEntrega;
+import com.algaworks.algalog.domain.repository.EntregaRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class SolicitacaoEntregaService {
+	private EntregaRepository entregaRepository;
+	private CatalogoClienteService clienteService;
+	
+	@Transactional
+	public Entrega solicitar(Entrega entrega) {
+		entrega.setStatus(StatusEntrega.PENDENTE);
+		entrega.setDataPedido(LocalDateTime.now());
+		entrega.setCliente(clienteService.buscar(entrega.getCliente().getId()));
+		
+		return entregaRepository.save(entrega);
+	};
+}
